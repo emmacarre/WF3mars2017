dbb2cf66acb4e22e3e7b7b3ec65d6a0644f6302d
Etudiant <Etudiant>
2017-03-31 12:36:52 +0200

    Single Page Application JQuery

--- 31 mars/exo1 - Single Page Application JS JQuery/js/script.js
+++ 31 mars/exo1 - Single Page Application JS JQuery/js/script.js
@@ -15,6 +15,15 @@ $(document).ready( function(){
         Contact: 'Contactez-moi pour plus d\'informations'
     }; 
 
+    // Créer un objet pour le contenu des pages
+    var myContent = {
+        Accueil: '<h3>Contenu de la page Accueil</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus suscipit hic, excepturi, voluptas id sapiente dignissimos. Repellat omnis sequi cumque, nam est nihil ad, maiores expedita nobis ratione eius, fuga.</p>',
+        Portfolio: '<h3>Contenu de la page Portfolio</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus suscipit hic, excepturi, voluptas id sapiente dignissimos. Repellat omnis sequi cumque, nam est nihil ad, maiores expedita nobis ratione eius, fuga.</p>',
+        Contact: '<h3>Contenu de la page Contact</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus suscipit hic, excepturi, voluptas id sapiente dignissimos. Repellat omnis sequi cumque, nam est nihil ad, maiores expedita nobis ratione eius, fuga.</p>'
+    }; 
+
+
+
     // Sélectionner le header et le mettre dans une variable
     var myHeader = $('header'); 
     
@@ -22,7 +31,8 @@ $(document).ready( function(){
     myHeader.append('<h1>' + siteTitle + '</h1>'); 
 
     // Générer une balise nav + ul dans le header
-    myHeader.append('<nav><ul></ul></nav>'); 
+    myHeader.append('<nav><i class="fa fa-bars" aria-hidden="true"></i><ul></ul></nav>'); 
+
 
     // Faire une boucle sur myNav pour générer les liens de la nav
     for( var i = 0; i <myNav.length; i++ ){
@@ -35,7 +45,8 @@ $(document).ready( function(){
 
     // Afficher dans le main le titre issu de l'objet myTitles
     var myMain = $('main'); 
-    myMain.append('<h2>' + myTitles.Accueil + '</h2>'); 
+    myMain.append('<h2>' + myTitles.Accueil + '</h2>');
+    myMain.append('<section>' + myContent.Accueil + '</section>');  
 
 
     // Capter l'évènement click sur les balises a en bloquant le comportement naturel des balises a 
@@ -45,11 +56,11 @@ $(document).ready( function(){
         evt.preventDefault(); 
 
         // Connaître l'occurrence de la balise a sur laquelle l'utilisateur a cliqué
-        console.log( $(this) ); 
+        // console.log( $(this) );
 
 
         // Récupérer la valeur de l'attribut href
-        console.log( $(this).attr('href') ); 
+        // console.log( $(this).attr('href') ); 
 
 
         // Vérifier la valeur de l'attribut href pour afficher le bon titre
@@ -57,14 +68,23 @@ $(document).ready( function(){
             
             // Sélectionner la balise h2 pour changer son contenu
             $('h2').text( myTitles.Accueil ); 
+
+            // Sélectionner la section pour changer le contenu
+            $('section').html( myContent.Accueil); 
         
         }else if( $(this).attr('href') == 'Portfolio' ){
              // Sélectionner la balise h2 pour changer son contenu
             $('h2').text( myTitles.Portfolio ); 
+
+            // Sélectionner la section pour changer le contenu
+            $('section').html( myContent.Portfolio); 
         
         }else{
             // Sélectionner la balise h2 pour changer son contenu
             $('h2').text( myTitles.Contact );  
+
+            // Sélectionner la section pour changer le contenu
+            $('section').html( myContent.Contact); 
         }; 
 
     }); 
